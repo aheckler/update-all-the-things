3cf6dc82701a9aff13c36c518ebb5e514a71e8f6
Adam Heckler <aheckler@fastmail.com>
2016-05-05 07:14:02 -0400

    Make script work in Hazel
    Remove "clear", specify full paths for everything, "echo" to log file,
    and open log file in Sublime Text when finished

--- update-all-the-things.sh
+++ update-all-the-things.sh
@@ -1,43 +1,44 @@
 #!/usr/local/bin/bash
-clear
 
 date=$(date +"%Y%m%d-%H%M%S")
-log_file=~/Desktop/update-log-$date.txt
+log_file=/Users/Adam/Desktop/update-log-$date.txt
 
-echo "Starting updates..."
-echo
+echo "Starting updates..."  &>> $log_file
+echo  &>> $log_file
 
-echo "==> Homebrew"
-echo "    Updating client"
-brew update &>> $log_file
-echo "    Updating packages"
-brew upgrade --all &>> $log_file
-echo "    Cleaning up"
-brew prune &>> $log_file
-brew cleanup --force &>> $log_file
-echo
+echo "==> Homebrew"  &>> $log_file
+echo "    Updating client"  &>> $log_file
+/usr/local/bin/brew update &>> $log_file
+echo "    Updating packages"  &>> $log_file
+/usr/local/bin/brew upgrade --all &>> $log_file
+echo "    Cleaning up"  &>> $log_file
+/usr/local/bin/brew prune &>> $log_file
+/usr/local/bin/brew cleanup --force &>> $log_file
+echo  &>> $log_file
 
-echo "==> Homebrew Cask"
-echo "    Cleaning up"
-brew cask cleanup &>> $log_file
-echo
+echo "==> Homebrew Cask"  &>> $log_file
+echo "    Cleaning up"  &>> $log_file
+/usr/local/bin/brew cask cleanup &>> $log_file
+echo  &>> $log_file
 
-echo "==> Atom Package Manager"
-echo "    Updating packages"
-apm update --no-confirm --no-color &>> $log_file
-echo "    Cleaning up"
-apm clean --no-color &>> $log_file
-apm dedupe --no-color &>> $log_file
-echo
+echo "==> Atom Package Manager"  &>> $log_file
+echo "    Updating packages"  &>> $log_file
+/usr/local/bin/apm update --no-confirm --no-color &>> $log_file
+echo "    Cleaning up"  &>> $log_file
+/usr/local/bin/apm clean --no-color &>> $log_file
+/usr/local/bin/apm dedupe --no-color &>> $log_file
+echo  &>> $log_file
 
-echo "==> Node Package Manager"
-echo "    Updating global packages"
-npm -g update &>> $log_file
-echo
+echo "==> Node Package Manager"  &>> $log_file
+echo "    Updating global packages"  &>> $log_file
+/usr/local/bin/npm -g update &>> $log_file
+echo  &>> $log_file
 
-echo "==> Composer"
-echo "    Updating client"
-composer self-update &>> $log_file
-echo
+echo "==> Composer"  &>> $log_file
+echo "    Updating client"  &>> $log_file
+/usr/local/bin/composer self-update &>> $log_file
+echo  &>> $log_file
+
+/usr/local/bin/subl $log_file
 
 exit 0
