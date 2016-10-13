900c38c0fe040f355bc6882394ea19d021bd4cd9
Adam Heckler <aheckler@fastmail.com>
2016-10-13 05:51:14 -0400

    Redirect only stdout to log file

--- update-all-the-things.sh
+++ update-all-the-things.sh
@@ -4,88 +4,108 @@
 #
 # https://github.com/aheckler/update-all-the-things
 
-# Create a log file
+################
+#  CREATE LOG  #
+################
+
 date=$(date +"%Y%m%d-%H%M%S")
 log_file=/Users/Adam/Desktop/update-log-$date.txt
 
-echo "Starting updates..." &>> $log_file
-echo &>> $log_file
+echo "Starting updates..." 1>> $log_file
+echo 1>> $log_file
 
 ##############
 #  HOMEBREW  #
 ##############
 
-echo "==> Homebrew" &>> $log_file
-echo "    Updating client" &>> $log_file
-brew update &>> $log_file
-echo "    Updating packages" &>> $log_file
-brew upgrade &>> $log_file
-echo "    Cleaning up" &>> $log_file
-brew prune &>> $log_file
-brew cleanup --force &>> $log_file
-echo &>> $log_file
+echo "==> Homebrew"
+echo "    Updating client"
+brew update 1>> $log_file
+
+echo "    Updating packages"
+brew upgrade 1>> $log_file
+
+echo "    Cleaning up"
+brew prune 1>> $log_file
+brew cleanup --force 1>> $log_file
 
 # "brew prune" deletes this directory, which breaks MySQL / MariaDB
 # https://github.com/Homebrew/legacy-homebrew/issues/31760
-mkdir /usr/local/etc/my.cnf.d &>> $log_file
+mkdir /usr/local/etc/my.cnf.d 1>> $log_file
+
+echo 1>> $log_file
 
 ###################
 #  HOMEBREW CASK  #
 ###################
 
-echo "==> Homebrew Cask" &>> $log_file
-echo "    Cleaning up" &>> $log_file
-brew cask cleanup &>> $log_file
-echo &>> $log_file
+echo "==> Homebrew Cask"
+echo "    Cleaning up"
+brew cask cleanup 1>> $log_file
+
+echo 1>> $log_file
 
 ##########
 #  ATOM  #
 ##########
 
-echo "==> Atom Package Manager" &>> $log_file
-echo "    Updating packages" &>> $log_file
-apm update --no-confirm --no-color &>> $log_file
-echo "    Cleaning up" &>> $log_file
-apm clean --no-color &>> $log_file
-apm dedupe --no-color &>> $log_file
-echo &>> $log_file
+echo "==> Atom Package Manager"
+echo "    Updating packages"
+apm update --no-confirm --no-color 1>> $log_file
+
+echo "    Cleaning up"
+apm clean --no-color 1>> $log_file
+apm dedupe --no-color 1>> $log_file
+
+echo 1>> $log_file
 
 #########
 #  NPM  #
 #########
 
-echo "==> Node Package Manager" &>> $log_file
-echo "    Updating global packages" &>> $log_file
-npm -g update &>> $log_file
-echo &>> $log_file
+echo "==> Node Package Manager"
+echo "    Updating global packages"
+npm -g update 1>> $log_file
+
+echo 1>> $log_file
 
 ##############
 #  COMPOSER  #
 ##############
 
-echo "==> Composer" &>> $log_file
-echo "    Updating client" &>> $log_file
-composer self-update &>> $log_file
-echo "    Updating global packages" &>> $log_file
-composer global update --no-progress &>> $log_file
-echo &>> $log_file
+echo "==> Composer"
+echo "    Updating client"
+composer self-update 1>> $log_file
+
+echo "    Updating global packages"
+composer global update --no-progress 1>> $log_file
+
+echo 1>> $log_file
 
 ###############
 #  RUBY GEMS  #
 ###############
 
-echo "==> Ruby Gems" &>> $log_file
-echo "    Updating sources" &>> $log_file
-gem sources --update &>> $log_file
-echo "    Updating system" &>> $log_file
-gem update --system &>> $log_file
-echo "    Updating gems" &>> $log_file
-gem update &>> $log_file
-echo "    Cleaning up" &>> $log_file
-gem cleanup --quiet &>> $log_file
-echo &>> $log_file
+echo "==> Ruby Gems"
+echo "    Updating sources"
+gem sources --update 1>> $log_file
 
-echo "ALL DONE!" &>> $log_file
+echo "    Updating system"
+gem update --system 1>> $log_file
+
+echo "    Updating gems"
+gem update 1>> $log_file
+
+echo "    Cleaning up"
+gem cleanup --quiet 1>> $log_file
+
+echo 1>> $log_file
+
+##############
+#  OPEN LOG  #
+##############
+
+echo "ALL DONE!" 1>> $log_file
 
 open -t $log_file
 
